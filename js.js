64b1f74be0cfe091e32e0d2a85346102b8b0f723
Taras Pikh <taraspih@gmail.com>
2017-09-28 12:45:52 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+
+$("Document").ready(function(){
+	$("body").css("background-color","#FFF182");
+ $(".resume").css("margin-top","50px");
+ $("span").css("font-weight","bold");
+ $(".tarasPhoto").css({"width":"110px"});
+ $(".resume").css({"margin-left":"250px", "margin-right":"200px", "background-color":"orange"});
+ $(".bottom").css({"margin-top":"20px","margin-bottom": "10px"});
+ });
+
